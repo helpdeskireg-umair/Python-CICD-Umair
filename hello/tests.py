ca5ab5a3f3a2aca8d740dbba8806f6d5cf456d8e
umair <helpdeskireg@gmail.com>
2026-08-13 11:48:17 +0000

    Initial commit: Django hello app

---
+++ hello/tests.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+
+class HomePageTests(SimpleTestCase):
+    def test_home_page_returns_200(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_contains_greeting(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Hello, Umair!")
---
+++ hello/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("<h1>Hello, Umair!</h1><p>This app was deployed with a CI/CD pipeline.</p>")
