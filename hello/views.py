3594d4bd17bda1470b40504e07d252b4727eb29a
umair <helpdeskireg@gmail.com>
2026-09-04 14:47:53 +0000

    cicd pipline cross check

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("<h1>Hello, Umair!</h1><p>This app was deployed with a CI/CD pipeline.</p>")
+    return HttpResponse("<h1>Hello, Umair Rao!</h1><p>This app was deployed with a CI/CD pipeline.</p>")
